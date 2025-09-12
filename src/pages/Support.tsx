c418b0dcd1be605bd84eda23598f8646e5b7effd
tolu <olufowobirasaq69@gmail.com>
2025-09-12 13:29:56 +0100

    added a support page

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -154,7 +154,7 @@ const Support = () => {
             <div className="bg-white/10 backdrop-blur-lg border border-white/20 rounded-2xl p-4 sm:p-6">
               <div className="flex flex-col sm:flex-row gap-4 mb-6">
                 <div className="flex-1 relative">
-                  <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+                  <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-200 z-10" />
                   <input
                     type="text"
                     placeholder="Search FAQs..."
